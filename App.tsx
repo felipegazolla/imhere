e0026a18287e10377877d877b49c24723d84aaaf
felipe <enggazolla@gmail.com>
2024-09-07 00:10:29 -0300

    stylesheet

--- App.tsx
+++ App.tsx
@@ -1,3 +1,34 @@
-export function App() {
-  
+import { Text, View, StyleSheet } from "react-native"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 9 de Setembro de 2024.
+      </Text>
+
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 56
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  }
+})
