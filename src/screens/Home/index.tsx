ba23912fe32a2ac07f4d9a1298f0773d4b50d0bd
felipe <enggazolla@gmail.com>
2024-09-07 21:32:49 -0300

    participant component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,19 @@
-import { Text, View, } from "react-native"
+import { Text, View, TextInput, TouchableOpacity } from "react-native"
+
+import { Participant } from "../../components/Participant"
 
 import { styles } from "./styles"
 
 export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("Voce clicou no botao de adicionar")
+  }
+
+  function handleParticipantRemove() {
+    console.log("Voce clicou no botao de remover")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -13,6 +24,29 @@ export function Home() {
         Sexta, 9 de Setembro de 2024.
       </Text>
 
+      <View
+        style={styles.form}
+      >
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#B0B3B5"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleParticipantAdd}
+        >
+          <Text
+          style={styles.buttonText}
+          >
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant name="Felipe" onRemove={handleParticipantRemove}/>
+      <Participant name="Mirian" onRemove={handleParticipantRemove}/>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,5 +15,33 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FDFCFE',
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 5,
+    backgroundColor: '#5A67D8',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
   }
 })
